be564b649ae4ea0a88d3a86f33f1ba9b83e95882
liangxj8 <liangxj8@gmail.com>
2017-07-17 22:08:20 +0800

    Score Show Upate
    Improve the score show and add more details

--- pyjw.py
+++ pyjw.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import re
-
 import requests
-
 from CASLogin import cas_login
+
+session = requests.Session()
 
 
 def get_kclb(kclb):
@@ -17,23 +17,46 @@ def get_score():
     resp = cas_login(url)
 
     url = re.findall(r'http://wjw.sysu.edu.cn/.*?&MU=', resp.text)[0]
-    session = requests.Session()
     session.get(url)
     form = {'year': '2016-2017', 'term': '2', 'pylb': '01'}
     return session.get('http://wjw.sysu.edu.cn/api/score', params=form, cookies=session.cookies).text
 
 
-def show_score():
-    result = eval(re.findall(r'\[.*\]', get_score())[0])
-    print('类型 课程 教师 学分 成绩 绩点 排名')
+def get_detail(resource_id):
+    form = {'resource_id': resource_id}
+    detail = session.get('http://wjw.sysu.edu.cn/api/score_detail', params=form, cookies=session.cookies).text
+    result = eval(re.findall(r'\[.*\]', detail)[0])
+    details = ""
     for ResultDict in result:
-        print(get_kclb(ResultDict['kclb']), end=' ')
-        print(ResultDict['kcmc'], end=' ')
-        print(ResultDict['jsxm'], end=' ')
-        print(ResultDict['xf'], end=' ')
-        print(ResultDict['zzcj'], end=' ')
-        print(ResultDict['jd'], end=' ')
-        print(ResultDict['jxbpm'].replace('\/', '/'), end='\n')
+        details += ResultDict['cjpdlb'] + '：' + ResultDict['fxyscj'] + '\n'
+    return details
+
+
+def show_score(result, resource_id=None):
+    if not result:
+        result = eval(re.findall(r'\[.*\]', get_score())[0])
+    if resource_id:
+        for ResultDict in result:
+            if resource_id == ResultDict['resource_id']:
+                break
+        msg = get_kclb(ResultDict['kclb']) + '：'
+        msg += ResultDict['kcmc'] + '\n'
+        msg += '绩点：' + ResultDict['jd'] + ' '
+        msg += '教学班排名：' + ResultDict['jxbpm'].replace('\/', '/') + '\n'
+        msg += '学分：' + ResultDict['xf'] + ' '
+        msg += '最终成绩：' + ResultDict['zzcj'] + '\n'
+        msg += get_detail(resource_id)
+        return msg.rstrip('\n')
+    else:
+        msg = '类型 课程 学分 成绩 绩点 排名'
+        for ResultDict in result:
+            msg += get_kclb(ResultDict['kclb']) + ' '
+            msg += ResultDict['kcmc'] + ' '
+            msg += ResultDict['xf'] + ' '
+            msg += ResultDict['zzcj'] + ' '
+            msg += ResultDict['jd'] + ' '
+            msg += ResultDict['jxbpm'].replace('\/', '/') + '\n'
+        return msg
 
 if __name__ == '__main__':
-    show_score()
+    print(show_score())
