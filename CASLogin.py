65e07d79278c6d35c6515c6d388e0dfea12b4844
liangxj8 <liangxj8@gmail.com>
2017-07-18 14:36:55 +0800

    Improve Login Process
    1. Avoid duplicate login using cookies.
    2. Add ConfigParser so that we need not to enter our account password
    every time.

--- CASLogin.py
+++ CASLogin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import requests
+import configparser
 from bs4 import BeautifulSoup
+
+session = requests.Session()
 
 
 def login(url, username=None, password=None):
@@ -8,25 +11,29 @@ def login(url, username=None, password=None):
         username = input('Please input your NetID: ')
     if not password:
         password = input('Please input your password: ')
-    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-    lt = soup.find('input', {'name': 'lt'})['value']
-    execution = soup.find('input', {'name': 'execution'})['value']
-    payload = {
-        'username': username,
-        'password': password,
-        'lt': lt,
-        'execution': execution,
-        '_eventId': 'submit',
-        'submit': '登录'
-    }
-    response = requests.post(url, data=payload)
-    return response
+    response = session.get(url, cookies=session.cookies)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    if not soup.find('input') or soup.find('h2').text == 'Log In Successful':
+        return response
+    else:
+        lt = soup.find('input', {'name': 'lt'})['value']
+        execution = soup.find('input', {'name': 'execution'})['value']
+        payload = {
+            'username': username,
+            'password': password,
+            'lt': lt,
+            'execution': execution,
+            '_eventId': 'submit',
+            'submit': '登录'
+        }
+        response = session.post(url, data=payload, cookies=session.cookies)
+        return response
 
 
 def cas_login(url, username, password):
     response = login(url, username, password)
     soup = BeautifulSoup(response.text, 'html.parser')
-    while soup.find(id='msg'):
+    while soup.find(id='msg') and (str(soup.find(id='msg').string) == 'Invalid credentials.'):
         print(soup.find(id='msg').string, end="\n\n")
         response = login(url)
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -35,4 +42,8 @@ def cas_login(url, username, password):
 
 
 if __name__ == '__main__':
-    cas_login('https://cas.sysu.edu.cn/cas/login')
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    NetID = config['CAS']['netid']
+    Password = config['CAS']['password']
+    cas_login('https://cas.sysu.edu.cn/cas/login', NetID, Password)
