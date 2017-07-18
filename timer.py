503df6ff693b36df36f57108bcd59a4b5e780470
liangxj8 <liangxj8@gmail.com>
2017-07-18 12:19:24 +0800

    Timed Task
    Make a schedule so that it can run at regular time.

---
+++ timer.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+import time
+import sched
+from main import main
+
+s = sched.scheduler(time.time, time.sleep)
+
+
+def perform(sec=60):
+    s.enter(sec, 0, perform, (sec,))
+    now = time.strftime('%Y.%m.%d %H:%M', time.localtime(time.time()))
+    print(now)
+    main()
+
+
+if __name__ == "__main__":
+    perform(600)
+    s.run()
